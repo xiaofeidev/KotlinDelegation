bb988e5a29e411efbf5d5dea15ae5e22b66f3bbc
liman <xiaofei.dev@gmail.com>
2020-07-03 20:13:21 +0800

    写 SP 功能完善

--- app/src/main/java/com/xiaofeidev/delegatedemo/delegates/SPDelegates.kt
+++ app/src/main/java/com/xiaofeidev/delegatedemo/delegates/SPDelegates.kt
@@ -12,11 +12,11 @@ import kotlin.reflect.KProperty
  * 具体使用此类定义 SP 存储项的代码请参考 SpBase 文件</p>
  */
 
-class SPDelegates<T>(private val key: String, private val default: T) : ReadWriteProperty<Any?, T> {
+class SPDelegates<T>(private val key: String, private val default: T, private val isCommit: Boolean = false) : ReadWriteProperty<Any?, T> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): T {
         return SPUtils.getValue(key, default)
     }
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-        SPUtils.putValue(key, value)
+        SPUtils.putValue(key, value, isCommit)
     }
 }
--- app/src/main/java/com/xiaofeidev/delegatedemo/delegates/SPUtils.kt
+++ app/src/main/java/com/xiaofeidev/delegatedemo/delegates/SPUtils.kt
@@ -1,5 +1,6 @@
 package com.xiaofeidev.delegatedemo.delegates
 
+import android.annotation.SuppressLint
 import android.content.Context
 import com.xiaofeidev.delegatedemo.App
 
@@ -14,13 +15,13 @@ object SPUtils {
     }
 
     //读 SP 存储项
-    fun <T> getValue(name: String, default: T): T = with(SP) {
+    fun <T> getValue(key: String, default: T): T = with(SP) {
         val res: Any = when (default) {
-            is Long -> getLong(name, default)
-            is String -> getString(name, default) ?: ""
-            is Int -> getInt(name, default)
-            is Boolean -> getBoolean(name, default)
-            is Float -> getFloat(name, default)
+            is Long -> getLong(key, default)
+            is String -> getString(key, default) ?: ""
+            is Int -> getInt(key, default)
+            is Boolean -> getBoolean(key, default)
+            is Float -> getFloat(key, default)
             else -> throw java.lang.IllegalArgumentException()
         }
         @Suppress("UNCHECKED_CAST")
@@ -28,14 +29,20 @@ object SPUtils {
     }
 
     //写 SP 存储项
-    fun <T> putValue(name: String, value: T) = with(SP.edit()) {
-        when (value) {
-            is Long -> putLong(name, value)
-            is String -> putString(name, value)
-            is Int -> putInt(name, value)
-            is Boolean -> putBoolean(name, value)
-            is Float -> putFloat(name, value)
+    @SuppressLint("ApplySharedPref")
+    fun <T> putValue(key: String, value: T, isCommit: Boolean = false) = with(SP.edit()) {
+        val editor = when (value) {
+            is Long -> putLong(key, value)
+            is String -> putString(key, value)
+            is Int -> putInt(key, value)
+            is Boolean -> putBoolean(key, value)
+            is Float -> putFloat(key, value)
             else -> throw IllegalArgumentException("This type can't be saved into Preferences")
-        }.apply()
+        }
+        if (isCommit){
+            editor.commit()
+        } else {
+            editor.apply()
+        }
     }
 }
